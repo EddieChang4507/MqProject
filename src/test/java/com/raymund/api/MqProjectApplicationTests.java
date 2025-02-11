44df50949367d851804e0e90d3be0c7508a49ed5
Raymund <eddiechang4507@gmail.com>
2025-02-11 10:45:47 +0800

    初始化專案

---
+++ src/test/java/com/raymund/api/MqProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raymund.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
